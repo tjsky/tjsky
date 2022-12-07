52232edbfdafa372ca5447ded93ecaaf2bc0b8e0
去年夏天 <tjsky@foxmail.com>
2022-12-07 14:20:24 +0800

    正式代码

--- mourn.js
+++ mourn.js
@@ -4,5 +4,5 @@
  *
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
  */
-var e;e=function(){const e=window.MOURNING_MODEDATE_LIST||["7/7","9/18","11/30","12/13","12/7"];window.addEventListener("load",(function(){const n=`${(new Date).getMonth()+1}/${(new Date).getDate()}`;e.some((e=>e===n))&&(document.body.style.filter="grayscale(1)")}))},"function"==typeof define&&define.amd?define(e):e();
+var e;e=function(){const e=window.MOURNING_MODEDATE_LIST||["7/7","9/18","11/30","12/13"];window.addEventListener("load",(function(){const n=`${(new Date).getMonth()+1}/${(new Date).getDate()}`;e.some((e=>e===n))&&(document.body.style.filter="grayscale(1)")}))},"function"==typeof define&&define.amd?define(e):e();
 //# sourceMappingURL=index.browser.js.map
