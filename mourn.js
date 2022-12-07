51d3bb8e84b20b0d8ab0ba9616055396799e6353
去年夏天 <tjsky@foxmail.com>
2022-12-07 14:14:50 +0800

    Update mourn.js

--- mourn.js
+++ mourn.js
@@ -1,20 +1,8 @@
-const dateList = [
-    '7/7', // 七七事变
-    '9/18', // 九一八事变
-    '12/13', // 国家公祭日
-    '12/2', // 测试
-]
-
-// 自动开启哀悼模式
-function autoEnableMourningMode() {
-    const m = new Date().getMonth() + 1
-    const d = new Date().getDate()
-    const today = `${m}/${d}`
-    if (dateList.some((date) => date === today)) {
-        document.body.style.filter = 'grayscale(1)'
-    }
-}
-
-window.addEventListener('load', () => {
-    autoEnableMourningMode()
-})
+/**
+ * Skipped minification because the original files appears to be already minified.
+ * Original file: /npm/mourning-mode@1.1.0/dist/index.browser.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+var e;e=function(){const e=window.MOURNING_MODEDATE_LIST||["7/7","9/18","11/30","12/13","12/7"];window.addEventListener("load",(function(){const n=`${(new Date).getMonth()+1}/${(new Date).getDate()}`;e.some((e=>e===n))&&(document.body.style.filter="grayscale(1)")}))},"function"==typeof define&&define.amd?define(e):e();
+//# sourceMappingURL=index.browser.js.map
